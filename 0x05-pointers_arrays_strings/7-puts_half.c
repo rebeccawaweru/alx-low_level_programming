f6fc4bb2dcd96b9ea0e6f55d017223eae82ddf63
rebeccawaweru <wawerur95@gmail.com>
2023-02-27 12:40:13 -0800

    correction

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,26 +1,28 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * puts_half - prints half of a string
- * @str: string
- * Return: void
- */
+ *
+ * @str: string parameter input
+ *
+ * Return: Nothing
+*/
 
 void puts_half(char *str)
 {
-	int i, j, max;
+	int l, n;
 
-	i = 0;
-	while (str[i] != '\0')
+	for (l = 0; str[l] != '\0'; ++l)
+		;
+
+	if (l % 2 == 0)
 	{
-		i++;
-	}
-	max = i;
-	j = max / 2;
-	while (j <= max)
+		for (n = l / 2; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+	} else
 	{
-		_putchar(str[j]);
-		j++;
+		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
+			_putchar(str[n]);
 	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,20 +10,14 @@
 
 void print_array(int *a, int n)
 {
-	int i, count;
+	int i;
 
-	i = 0;
-	count = 1;
-	if (n < 0)
-		n = 0;
-	if (n > 0)
+	for (i = 0; i < n; ++i)
 	{
-		while (a[i] != '\0' && count < n)
-		{
+		if (i != (n - 1))
 			printf("%d, ", a[i]);
-			i++;
-			count++;
-		}
-		printf("%d\n", a[i]);
+		else
+			printf("%d", a[i]);
 	}
+	printf("\n");
 }
