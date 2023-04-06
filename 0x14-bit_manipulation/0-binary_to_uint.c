a929ed89df71aaa3f546f6ade12b090c32ef3fac
rebeccawaweru <wawerur95@gmail.com>
2023-04-06 06:45:21 -0700

    task0 correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,11 +11,12 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	int j;
 	unsigned int number = 0;
 
 	if (b == NULL)
 		return (0);
-	for (int j = 0; b[j] != '\0'; j++)
+	for (j = 0; b[j] != '\0'; j++)
 	{
 		if (b[j] == '0')
 		{
