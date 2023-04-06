b5c9711b3ed77f388bf87422a2cf07da57e8d470
rebeccawaweru <wawerur95@gmail.com>
2023-04-06 07:53:53 -0700

    task 2.10

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - function returns value of bit in an index
+ * @n: is an interger
+ * @index: is an index
+ * Return: interger
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	while (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	return ((n >> index) & 1);
+}
