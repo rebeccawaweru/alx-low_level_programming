c7bc799b1fd75264a86630970347e1b39dc96422
rebeccawaweru <wawerur95@gmail.com>
2023-04-10 04:06:20 -0700

    task0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+#define SIZE_OF_BUFFER 1024
+
+/**
+ * read_textfile - function that reads text file and prints to POSIX
+ * @letters: number of letters to be read and printed
+ * @filename: the name of the file
+ *
+ * Return: letters if read and printed,
+ * 0 if filename is NULL  or file not read or opened or write fails
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f; /*refers to the file descriptor*/
+	/* maximum size of data we can read from a file in a single operation */
+	char buffer[SIZE_OF_BUFFER];
+	/* total is total number of bytes noread and nowritten*/
+	ssize_t noread, nowritten, total = 0;
+	/* first check if filename is NULL */
+	if (filename == NULL)
+		return (0);
+	/* use open() to open the file */
+	/* use O_RDONLY - to read the contents of the file */
+	f = open(filename, O_RDONLY);
+
+	/* f will have a negative value if the file has no contents*/
+	/* or if an error ocurred when trying to open*/
+	if (f < 0)
+		return (0);
+	/* Read and write the contents of the file in chunks of */
+	/* SIZE_OF_BUFFER bytes */
+	while ((noread = read(f, buffer, SIZE_OF_BUFFER)) > 0 && total < (ssize_t) letters)
+	{
+		/*write buffer contents to the standard output*/
+		/*use STDOUT_FILENO - POSIX symbolic constant*/
+		nowritten = write(STDOUT_FILENO, buffer, noread);
+		/*check for error when writing standard output*/
+		if (nowritten < 0)
+			return (0);
+		/*update the total letters read and printed*/
+		total += nowritten;
+	}
+	/*check for errors in read opearation*/
+	if (noread < 0)
+		return (0);
+	/*finally close the file)*/
+	close(f);
+	/*Return number of letters read and printed*/
+	return (total);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/*
+ * Guards ensure header file is included only once and there are no duplicates.
+ * Multiple inclusions of the same declarations can cause errors
+ * and conflicts
+ */
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
