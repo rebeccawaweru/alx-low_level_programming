53c5cfc0aae25aa9ea196ad01dea0517a812a91f
rebeccawaweru <wawerur95@gmail.com>
2023-05-18 06:08:57 -0700

    free lists

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free the list
+ * @head: start of the node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	if (head)
+	{
+		current = head->next;
+		free(head);
+		head = current;
+	}
+}
